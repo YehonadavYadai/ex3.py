1b78c99e85912c94751b67b8ac744e6e0ec1c52f
rafasu4 <67747529+rafasu4@users.noreply.github.com>
2020-12-30 13:40:51 +0200

    graph interface

---
+++ TestDiGraph.py
@@ -0,0 +1,13 @@
+import unittest
+import DiGraph
+
+
+class MyTestCase(unittest.TestCase):
+    g = 
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
