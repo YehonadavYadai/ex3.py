4a069611c1ccab4f38b5de8dba1a04c7db870d01
rafasu4 <67747529+rafasu4@users.noreply.github.com>
2020-12-31 15:59:16 +0200

    Delete tr.py

--- tr.py
+++
@@ -1,9 +0,0 @@
-from Ex3.DiGraph import DiGraph
-
-if __name__ == '__main__':
-    dict = {1:'a', 2:'b'}
-    for key in dict:
-        c=dict[key]
-        print(c)
-        # current_edge = edge_dic[i]
-
